baaa808f0a43efb0a5ce2763531670c93c3b3864
philips <philencripted@gmail.com>
2019-12-26 20:29:31 +0100

    update

--- config/env/production.js
+++ config/env/production.js
@@ -45,7 +45,7 @@ module.exports = {
      ***************************************************************************/
     default: {
       adapter: "sails-mongo",
-      url: process.env.MONGODB_URI
+      url: process.env.MONGODB_URI,
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
@@ -66,7 +66,7 @@ module.exports = {
        * https://sailsjs.com/config/datastores                                     *
        *                                                                           *
        ****************************************************************************/
-      // ssl: true
+      ssl: true
     }
   },
 
