fd7ce649acca2842205b1c5d2e04b33be5f5bc4c
philips <philencripted@gmail.com>
2019-12-26 23:12:58 +0100

    update

--- api/controllers/note/getnotes.js
+++ api/controllers/note/getnotes.js
@@ -6,9 +6,11 @@
  */
 
 module.exports = async function getNotes(req, res) {
-  const allNotes = await Note.find().sort("createdAt DESC");;
+  const allNotes = await Note.find()
+    .fetch()
+    .sort("createdAt DESC");
   if (!allNotes) {
     return res.status(404).json({ error: "could not retrieve notes" });
   }
-  return res.json(allNotes);
+  return res.status(200).json(allNotes);
 };
